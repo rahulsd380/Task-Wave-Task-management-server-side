1a972128d182e0e51f0358f0ba5a916655daabe0
rahulsd380 <rahulsd380@gmail.com>
2024-01-26 22:43:39 +0600

    users info save to database

--- index.js
+++ index.js
@@ -31,11 +31,7 @@ async function run() {
     const taskCollection = client.db("taskManagement").collection('tasks');
     // const allProductsCollection = client.db("taskManagement").collection('allProducts');
 
-    app.post('/users', async(req, res)=> {
-        const user = req.body;
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-    })
+
 
 
 
